a7be60da848a5fb43178d6c975a9a4124a53fa19
Cristobal Morales <mynameisjeffitowastaken@gmail.com>
2024-03-02 19:17:46 -0600

    tmdb fix

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ window.__CONFIG__ = {
   VITE_CORS_PROXY_URL: "https://lovely-pika-20c763.netlify.app",
 
   // The READ API key to access TMDB
-  VITE_TMDB_READ_API_KEY: "CHANGEME",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkZjk3N2IyNTU1NDM1MzkzZTE0MmI0ODZlYzU5ZTRmOSIsInN1YiI6IjY1ZTNjODg3OTQ1MWU3MDE4NzU5Y2IwMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.We82pbEdyPqiChcqH-BFN4yCNlCSGfYOBMpBbCMUuuw",
 
   // The DMCA email displayed in the footer, null to hide the DMCA link
   VITE_DMCA_EMAIL: "mynameisjeffitowastaken@gmail.com",
